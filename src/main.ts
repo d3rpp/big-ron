ead5828326f4e82afe9f394e6792d3968bebd6b9
Dylan Hackworth <dylhack@outlook.com>
2022-07-28 17:03:28 -0500

    make initial getUpdates

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ const TEN_MINUTES = 10 * 60 * 1000;
 async function main(): Promise<void> {
   await startBot();
 
+  await getUpdates(post);
   setInterval(() => {
     getUpdates(post);
   }, TEN_MINUTES);
