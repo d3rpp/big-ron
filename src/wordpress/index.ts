fe043ab6681af3e23d334bfc5b800238f9a00538
d3rpp <hudson.curren@icloud.com>
2022-07-29 10:46:48 +1200

    it works now

--- src/wordpress/index.ts
+++ src/wordpress/index.ts
@@ -6,7 +6,7 @@ import { User } from '../types/wordpress/User';
 import { ArticlePost } from './article';
 
 // https://developer.wordpress.org/rest-api/using-the-rest-api/global-parameters/
-const API_QUERY_FIELDS = 'id,date_gmt,yoast_head_json.og_title,yoast_head_json.og_description,yoast_head_json.canonical,yoast_head_json.og_image[].url,yoast_head_json.author,date';
+const API_QUERY_FIELDS = 'id,date_gmt,yoast_head_json.og_title,yoast_head_json.og_description,yoast_head_json.canonical,yoast_head_json.og_image,yoast_head_json.author,date';
 
 // eslint-disable-next-line max-len, no-async-promise-executor, import/prefer-default-export
 export const getArticleDetails = (APIRoot: string, postId: string): Promise<ArticlePost> => new Promise<ArticlePost>(async (resolve, reject) => {
@@ -18,9 +18,8 @@ export const getArticleDetails = (APIRoot: string, postId: string): Promise<Arti
     const articleResponseTyped = (await articleResponse.json()) as Article;
 
     // Get Author Props
-    const authorResponse = await fetch(
-      `${APIRoot}/wp-json/wp/v2/users/${articleResponseTyped.author}?_fields=name,avatar_urls`,
-    );
+    // eslint-disable-next-line no-underscore-dangle
+    const authorResponse = await fetch(articleResponseTyped._links.author[0].href);
 
     const authorResponseTyped = (await authorResponse.json()) as User;
 
