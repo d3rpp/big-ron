4668a7510b32d9d956fec77c7e503a40fe9904d5
Dylan Hackworth <dylhack@outlook.com>
2022-07-28 13:20:21 -0500

    feat(captions): respect bold tags

--- src/wordpress/util.ts
+++ src/wordpress/util.ts
@@ -1,5 +1,7 @@
 import * as he from 'he';
 
 export default function decodeHe(context: string): string {
-  return he.decode(context);
+  // respect bold
+  const raw = context.replace(/(&#822[01];)/g, '**');
+  return he.decode(raw);
 }
