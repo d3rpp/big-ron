0d7a5b1dc46da38d9bc03990295d688481544a78
d3rpp <hudson.curren@icloud.com>
2022-07-29 09:45:26 +1200

    removed unecessary commented import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import startBot from './bot';
 import post from './bot/posts';
 import { getUpdates } from './wordpress/update';
-// import decodeHe from './wordpress/util';
 
 const TEN_MINUTES = 10 * 60 * 1000;
 
